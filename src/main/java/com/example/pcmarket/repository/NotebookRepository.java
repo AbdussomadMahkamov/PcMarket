8f3871f2f37f9df5f8edb3f3acd6e88b9c2f8ed3
AbdussomadMahkamov <mahkamovabdussomad@gmail.com>
2022-12-15 23:21:21 +0500

    v0.06

--- src/main/java/com/example/pcmarket/repository/NotebookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pcmarket.repository;
-
-import com.example.pcmarket.entity.Notebook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotebookRepository extends JpaRepository<Notebook, Integer> {
-    boolean existsByVidekartaAndEkranDioganalAndEkranOlchamiAndProtsessorAndOperativkaAndXotiraAndOperatsionSistema(String videkarta, String ekranDioganal, String ekranOlchami, String protsessor, String operativka, String xotira, String operatsionSistema);
-}
